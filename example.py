3d0bda9d2c3b7119bfc911de6f638ab2c101d9c1
IvanKirpichnikov <mmssvvvv570@gmail.com>
2024-09-18 00:13:37 +0300

    add example

---
+++ example.py
@@ -0,0 +1,21 @@
+import asyncio
+from datetime import datetime
+
+from diary.diary import create_diary
+
+
+BEARER_TOKEN = ''
+
+
+async def main():
+    async with await create_diary(BEARER_TOKEN) as diary:
+        print(
+            await diary.get_short_schedules(
+                [datetime.now().date()]
+            )
+        )
+        print(await diary.get_student_profile())
+        print(await diary.get_user_info())
+
+
+asyncio.run(main())
